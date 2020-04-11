50a9b32d44c325fe4634fac9afd1b7db387670e3
Muhammad Umair <muhammad.umair@granjur.net>
2020-04-12 03:13:48 +0500

    updating shop , collection preview  component  and collection item component

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = SHOP_DATA;
+  }
+  render() {
+    const collections = this.state;
+    return collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ));
+  }
+}
+export default ShopPage;
